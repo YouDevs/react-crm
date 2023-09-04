62a688439e55ea5bea6167786d8ee5a8b4618d50
YouDevs <carlos_develops@outlook.com>
2023-09-04 16:42:51 -0600

    resaltar página actual

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,29 @@
-import { Outlet, Link } from 'react-router-dom'
+import { Outlet, Link, useLocation, NavLink } from 'react-router-dom'
 
 function Layout() {
+
+    const location = useLocation()
+
     return (
         <div className='md:flex md:min-h-screen'>
             <aside className='md:w-1/4 bg-blue-900 px-5 py-10'>
                 <h2 className='text-4xl font-black text-center text-white'>CRM Cliente</h2>
 
                 <nav className='mt-10'>
-                    <Link className='text-2xl block mt-2 hover:text-blue-300 text-white' to="/">Clientes</Link>
-                    <Link className='text-2xl block mt-2 hover:text-blue-300 text-white' to="/clientes/nuevo">NuevoClientes</Link>
+                    <Link
+                        className={`${location.pathname === '/'? 'text-blue-300': 'text-white'} text-2xl block mt-2 hover:text-blue-300`}
+                        to="/">
+                        Clientes
+                    </Link>
+                    <Link
+                        className={`${location.pathname === '/clientes/nuevo'? 'text-blue-300': 'text-white'} text-2xl block mt-2 hover:text-blue-300`}
+                        to="/clientes/nuevo">
+                        Nuevo Cliente
+                    </Link>
+                    <NavLink
+                        className={ ({isActive}) => isActive ? 'text-blue-300': 'text-white' }>
+                        Nuevo Cliente
+                    </NavLink>
                 </nav>
                         
             </aside>
